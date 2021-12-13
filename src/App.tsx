e2299a9f981c33ebdd1c2bf67bbadaf3e7e2cf34
Your Name <you@example.com>
2021-12-13 10:41:30 +0300

    feat: routing

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UserLoginPage from './Pages/LoginPage/UserLoginPage';
+import UserListPage from './Pages/UserListPage/UserListPage';
+
+const App = () => {
+  const isLoggedIn = () => {
+    const token =
+      localStorage.getItem('token') || sessionStorage.getItem('token');
+    return !!token;
+  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserLoginPage />} />
+        <Route path="/users" element={isLoggedIn() ? <UserListPage /> : null} />
+        <Route path="*" element={<UserLoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
