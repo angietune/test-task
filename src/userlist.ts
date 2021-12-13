a6dd7216536400936dee2dcbd16642b6118681b4
Your Name <you@example.com>
2021-12-13 10:40:06 +0300

    feat: fake DB

---
+++ src/userlist.ts
@@ -0,0 +1,36 @@
+const userlist = [
+  {
+    id: 'kctest0202',
+    name: 'Bernardo Santini',
+  },
+  {
+    id: 'kctest0203',
+    name: 'Bernardo Santini',
+  },
+  {
+    id: 'kctest0204',
+    name: 'Bernardo Santini',
+  },
+  {
+    id: 'kctest0205',
+    name: 'Bernardo Santini',
+  },
+  {
+    id: 'kctest0206',
+    name: 'Bernardo Santini',
+  },
+  {
+    id: 'kctest0207',
+    name: 'Bernardo Santini',
+  },
+  {
+    id: 'kctest0208',
+    name: 'Bernardo Santini',
+  },
+  {
+    id: 'kctest0209',
+    name: 'Bernardo Santini',
+  },
+];
+
+export default userlist;
